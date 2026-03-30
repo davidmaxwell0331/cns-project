d79b44b8b996739444502ee208c59f49c8081d3c
xavie <xaviewavie@live.com>
2026-03-29 21:09:23 -0500

    added project skeleton with all function assignments

---
+++ project.py
@@ -0,0 +1,39 @@
+# CNS1001 - Introduction to Programming
+# Project 2
+# student grade tracker
+# members : Xavier Subratie   (2509139) - project lead, architecture
+#           Ethan Eubanks     (2509775) - lead dev (core logic)
+#           Rodaine Wuarrie   (2409674) - dev (data & validation)
+#           Rajali Burrell    (2403230) - QA & Testing lead
+#           Mikayliea Edwards (2403230) - documentation lead
+#           David Maxwell     (insert your id here, david) - presentation & demo lead
+
+# main data structure ~ holds everything, list of directories.
+students_data []
+
+# --- data & validation functions (Rodaine Quarrie) ---
+# TODO: add_student(name, student_id)
+# TODO: add_grade(student_id, subject, score)
+# TODO: update_grade(student_id, subject, new_score)
+# TODO: validate_input(prompt, input_type, min_val, max_val)
+
+# --- core logic functions (Ethan Eubanks) ---
+# TODO: display_menu()
+# TODO: calculate_average(student)
+# TODO: assign_letter_grade(average)
+# TODO: display_student_report(student_id)
+
+# --- search & utility functions (Xavier Subratie) ---
+# TODO: search_student(query)
+# TODO: class_summary()
+
+# --- file handing functions (David Maxwell) ---
+# TODO: save_to_file(filename)
+# TODO: load_from_file(filename)
+
+# --- main function (Ethan Eubanks) ---
+# TODO: main()
+
+# --- Program Entry Point ---
+# if __name__ == "__main__":
+#     main()
